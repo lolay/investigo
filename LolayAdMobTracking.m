1e93a25545d30d0367becfecc0b724398dd2514e
Gary Rudolph <GaryRudolph@me.com>
2011-05-25 00:40:51 -0700

    Fixed admob reporting to be synchronous and to use the defaults rather than a file

--- LolayAdMobTracking.m
+++ LolayAdMobTracking.m
@@ -5,40 +5,20 @@
 #import <CommonCrypto/CommonDigest.h>
 #import "LolayAdMobTracking.h"
 
-#define ADMOB_FILE_NAME @"AdMobGreatSuccess"
+#define ADMOB_NAME @"AdMobGreatSuccess"
 #define ADMOB_URL @"http://a.admob.com/f0?isu=%@&md5=1&app_id=%@"
-
-@interface LolayAdMobTracking ()
-
-@property (nonatomic, retain, readwrite) NSMutableData* receivedData;
-@property (nonatomic, retain, readwrite) NSHTTPURLResponse * lastResponse;
-
-@end
 
 @implementation LolayAdMobTracking
 
-@synthesize receivedData = receivedData_;
-@synthesize lastResponse = lastResponse_;
-
-#pragma mark -
-#pragma mark Init
-- (id) init {
-	if (self = [super init] ) {
-		self.receivedData = [[NSMutableData data] retain];
-		self.lastResponse = nil;
-	}
-	return self;	
-}
-
 #pragma mark -
 #pragma mark Helpers
-- (NSString *)hashedISU {
-	NSString *result = nil;
-	NSString *isu = [UIDevice currentDevice].uniqueIdentifier;
+- (NSString*) hashedISU {
+	NSString* result = nil;
+	NSString* isu = [UIDevice currentDevice].uniqueIdentifier;
 	
 	if(isu) {
 		unsigned char digest[16];
-		NSData *data = [isu dataUsingEncoding:NSASCIIStringEncoding];
+		NSData* data = [isu dataUsingEncoding:NSASCIIStringEncoding];
 		CC_MD5([data bytes], [data length], digest);
 		
 		result = [NSString stringWithFormat: @"%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x",
@@ -55,65 +35,25 @@
 	return result;
 }
 
-- (NSString*) adMobFilePath {
-	NSString *documentsDirectory = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory,NSUserDomainMask, YES) objectAtIndex:0];
-	NSString *appOpenPath = [documentsDirectory stringByAppendingPathComponent:ADMOB_FILE_NAME];
-	return appOpenPath;
-}
-
-- (BOOL) adMobFileExists {
-	NSFileManager *fileManager = [NSFileManager defaultManager];
-	if([fileManager fileExistsAtPath:[self adMobFilePath]]) {
-		return YES;
-	}
-	return NO;
-}
-
-- (void) writeAdMobFile {
-	NSFileManager *fileManager = [NSFileManager defaultManager];
-	[fileManager createFileAtPath:[self adMobFilePath] contents:nil attributes:nil];
-}
-
 #pragma mark -
-#pragma mark NSURLConnection Delegate Methods
-- (void)connection:(NSURLConnection *)connection didReceiveResponse:(NSHTTPURLResponse *)response {
-	if (self.lastResponse != nil) {
-		[self.lastResponse release];
-	}
-	
-	self.lastResponse = [response retain];
-}
-
-- (void)connection:(NSURLConnection *)connectiondidFailWithError:(NSError *)error {
-	NSLog(@"[LolayAdMobTracking connectiondidFailWithError] error=%@",[error localizedDescription]);
-}
-
-- (void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data {
-	[self.receivedData appendData:data];
-}
-
-- (void)connectionDidFinishLoading:(NSURLConnection *)connection {
-	if (self.lastResponse && [self.lastResponse statusCode] == 200 && ([self.receivedData length] > 0)) {
-		[self writeAdMobFile];
-	}
-}
-
 #pragma mark -
 #pragma mark Report Method
-- (void) report:(NSString*)iTunesId {
-	if(![self adMobFileExists]) {
-		NSString *appOpenEndpoint = [NSString stringWithFormat:ADMOB_URL, [self hashedISU], iTunesId];
-		NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:appOpenEndpoint]];
-		NSURLConnection *connection = [[NSURLConnection alloc] initWithRequest:request delegate:self startImmediately:YES];
-		[connection release];
+- (void) report:(NSString*) iTunesId {
+	if(! [[NSUserDefaults standardUserDefaults] boolForKey:ADMOB_NAME]) {
+		NSLog(@"Reporting to admob");
+		NSString* appOpenEndpoint = [NSString stringWithFormat:ADMOB_URL, [self hashedISU], iTunesId];
+		NSURLRequest* request = [NSURLRequest requestWithURL:[NSURL URLWithString:appOpenEndpoint]];
+		NSURLResponse* response = nil;
+		NSError* error = nil;
+		NSData* responseData = [NSURLConnection sendSynchronousRequest:request returningResponse:&response error:&error];
+		
+		if ((! error) && ([(NSHTTPURLResponse*) response statusCode] == 200) && ([responseData length] > 0)) {
+			[[NSUserDefaults standardUserDefaults] setBool:YES forKey:ADMOB_NAME];
+			[[NSUserDefaults standardUserDefaults] synchronize];
+		}
+	} else {
+		NSLog(@"Already reported to admob, skipping");
 	}
-}
-
-- (void) dealloc {
-	self.receivedData = nil;
-	self.lastResponse = nil;
-	
-	[super dealloc];
 }
 
 @end
