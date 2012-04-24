b63a7138ffc7d9bb88522af7304609c381c8b315
GaryRudolph <GaryRudolph@mac.com>
2012-04-24 13:48:22 -0700

    Added No Tracker

---
+++ LolayNoTracker.h
@@ -0,0 +1,10 @@
+//
+//  Created by Lolay, Inc.
+//  Copyright 2011 Lolay, Inc. All rights reserved.
+//
+
+#import "LolayBaseTracker.h"
+
+@interface LolayNoTracker : LolayBaseTracker
+
+@end
---
+++ LolayNoTracker.m
@@ -0,0 +1,10 @@
+//
+//  Created by Lolay, Inc.
+//  Copyright 2011 Lolay, Inc. All rights reserved.
+//
+
+#import "LolayNoTracker.h"
+
+@implementation LolayNoTracker
+
+@end
