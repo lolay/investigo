6bf0e36caf6ccfca773eded436b30b98bf712543
Tony Stone <hatter24@gmail.com>
2015-10-07 13:13:09 -0700

    Adding umbrella header 'LolayInvestigo.h"

---
+++ LolayInvestigo/LolayInvestigo.h
@@ -0,0 +1,25 @@
+//
+//  LolayInvestigo.h
+//  Pods
+//
+//  Created by Tony Stone on 10/7/15.
+//
+//
+
+#ifndef LolayInvestigo_h
+#define LolayInvestigo_h
+
+#import <UIKit/UIKit.h>
+
+#import "LolayAnalyticsTracker.h"
+#import "LolayBaseTracker.h"
+#import "LolayFlurryTracker.h"
+#import "LolayInvestigoGlobals.h"
+#import "LolayLocaleTracker.h"
+#import "LolayMultipleTracker.h"
+#import "LolayNoTracker.h"
+#import "LolayNSLogTracker.h"
+#import "LolayTracker.h"
+#import "LolayTrackerGender.h"
+
+#endif /* LolayInvestigo_h */
